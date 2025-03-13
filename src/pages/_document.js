f028e9d77d4187604e42a44a581791c2ac23ec50
MleinaD <medveczkydaniel@gmail.com>
2025-03-13 13:50:29 +0100

    Add analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,11 +10,22 @@ export default function Document({ children }) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-44FTJRWJN0"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-44FTJRWJN0');
+            `
+          }}
+        />
       </Head>
-        <body className="bg-dark-700">
-          <Main />
-          <NextScript />
-        </body>
+      <body className="bg-dark-700">
+        <Main />
+        <NextScript />
+      </body>
     </Html>
   )
 }
